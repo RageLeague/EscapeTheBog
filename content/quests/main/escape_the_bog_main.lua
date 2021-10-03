a0a63fd2c2cbd6c43bca5009109d5f1eb4a97cbb
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-02 20:37:43 -0400

    Add core generation logic

--- content/quests/main/escape_the_bog_main.lua
+++ content/quests/main/escape_the_bog_main.lua
@@ -9,15 +9,46 @@ local QDEF = QuestDef.Define
 
         TheGame:GetGameState():SetMainQuest(quest)
 
-        quest:DefFn("GenerateBogMap", 30, {"ETB_LOC_STARTING_BOGCAVE", "ETB_LOC_DEEP_BOG"})
+        quest:DefFn("GenerateBogMap", 30, "ETB_LOC_STARTING_BOGCAVE")
     end,
 
-    GenerateBogMap = function(quest, map_count, mandatory_locations)
+    GenerateBogMap = function(quest, map_count, starting_location, mandatory_locations)
+        map_count = map_count or 0
         local quest_queue = {}
-        for i, id in ipairs(mandatory_locations or {}) do
-            table.insert(quest_queue, QuestUtil.SpawnQuest(id))
+        -- for i, id in ipairs(mandatory_locations or {}) do
+        --     table.insert(quest_queue, QuestUtil.SpawnQuest(id))
+        -- end
+        table.insert(quest_queue, QuestUtil.SpawnQuest(starting_location))
+        -- print(quest_queue[1]:DefFn("CanAttachLocation", quest_queue[2]))
+        -- print(quest_queue[1]:DefFn("AttachLocation", quest_queue[2]))
+        quest.param.current_location = quest_queue[1]
+        local tag_cache = {}
+        while map_count > 0 do
+            map_count = map_count - 1
+            local idx = math.random(1, #quest_queue)
+            local chosen_quest = quest_queue[idx]
+            chosen_quest:DefFn("SetupLocationData")
+            local chosen_tag = table.arraypick(chosen_quest.param.available_exits)
+            if not tag_cache[chosen_tag] then
+                tag_cache[chosen_tag] = {}
+                for i, id in ipairs(EscapeTheBogUtil.BOG_LOCATION_QUESTS) do
+                    local qdef = Content.GetQuestDef( id )
+                    if not qdef.is_unique then
+                        local loc_id = string.format( "%s_main_location", qdef.id)
+                        if chosen_tag == "any" or table.arraycontains(Content.GetLocationContent(loc_id).tags or {}, chosen_tag) then
+                            table.insert(tag_cache[chosen_tag], id)
+                        end
+                    end
+                end
+            end
+            if #tag_cache[chosen_tag] > 0 then
+                local new_quest = QuestUtil.SpawnQuest(starting_location)
+                chosen_quest:DefFn("AttachLocation", new_quest)
+                quest_queue[idx] = new_quest
+                if #chosen_quest.param.available_exits > 0 then
+                    table.insert(quest_queue, chosen_quest)
+                end
+            end
         end
-        print(quest_queue[1]:DefFn("CanAttachLocation", quest_queue[2]))
-        print(quest_queue[1]:DefFn("AttachLocation", quest_queue[2]))
     end,
 }
