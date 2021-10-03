cab375e6b28e13fe5bea1ab765374651f575d163
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-02 20:54:51 -0400

    Update escape_the_bog_main.lua

--- content/quests/main/escape_the_bog_main.lua
+++ content/quests/main/escape_the_bog_main.lua
@@ -9,15 +9,12 @@ local QDEF = QuestDef.Define
 
         TheGame:GetGameState():SetMainQuest(quest)
 
-        quest:DefFn("GenerateBogMap", 30, "ETB_LOC_STARTING_BOGCAVE")
+        quest:DefFn("GenerateBogMap", 10, "ETB_LOC_STARTING_BOGCAVE")
     end,
 
     GenerateBogMap = function(quest, map_count, starting_location, mandatory_locations)
         map_count = map_count or 0
         local quest_queue = {}
-        -- for i, id in ipairs(mandatory_locations or {}) do
-        --     table.insert(quest_queue, QuestUtil.SpawnQuest(id))
-        -- end
         table.insert(quest_queue, QuestUtil.SpawnQuest(starting_location))
         -- print(quest_queue[1]:DefFn("CanAttachLocation", quest_queue[2]))
         -- print(quest_queue[1]:DefFn("AttachLocation", quest_queue[2]))
@@ -48,6 +45,36 @@ local QDEF = QuestDef.Define
                 if #chosen_quest.param.available_exits > 0 then
                     table.insert(quest_queue, chosen_quest)
                 end
+            end
+        end
+        -- Linking mandatory locations to existing ones
+        for i, id in ipairs(mandatory_locations or {}) do
+            table.shuffle(quest_queue)
+            local spawned_quest = QuestUtil.SpawnQuest(id)
+            local ok = false
+            for i, target_quest in ipairs(quest_queue) do
+                if spawned_quest:DefFn("AttachLocation", target_quest) then
+                    ok = true
+                    break
+                end
+            end
+            assert(ok, loc.format("No entry generated for {1}", spawned_quest))
+            table.insert(quest_queue, spawned_quest)
+        end
+        -- At this point everything should be connected.
+        -- Now let's add alternative routes, probably
+        while #quest_queue > 1 then
+            table.shuffle(quest_queue)
+            local quest_to_connect = quest_queue[#quest_queue]
+            local ok = false
+            for i = #quest_queue - 1, 1, -1 do
+                if quest_to_connect:DefFn("AttachLocation", quest_queue[i]) then
+                    ok = true
+                    break
+                end
+            end
+            if not ok or #quest_to_connect.param.available_exits == 0 then
+                quest_queue[#quest_queue] = nil
             end
         end
     end,
