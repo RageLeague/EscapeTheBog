2dbc40522d408e340a6e55d0fb86fe8c6687b435
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-03 11:00:33 -0400

    Update convo_loc_common.lua

--- content/convo_loc_common.lua
+++ content/convo_loc_common.lua
@@ -1,5 +1,6 @@
 Content.AddStringTable( "ETB_CONVO_COMMON", {
     CONVO_COMMON = {
+        OPT_TRAVEL = "Travel",
         OPT_MOVE_TO = "Travel to {1#location}",
         DIALOG_MOVE_TO = [[
             * You walk towards {1#location}.
