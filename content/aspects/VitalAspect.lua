2666afdceb362bcac0796e0ce09a953731a022f4
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-07 19:26:38 -0400

    Add hunger aspect

--- content/aspects/VitalAspect.lua
+++ content/aspects/VitalAspect.lua
@@ -1,3 +1,4 @@
+-- Define basis for all Vital Aspects
 local VitalAspect = class( "ETBClass.VitalAspect", Aspect)
 
 function VitalAspect:init( agent )
@@ -12,6 +13,15 @@ end
 
 function VitalAspect:GetLocalizedString( key )
     return LOC(self:GetLocStringPrefix(key))
+end
+
+-- Returns up to two values:
+-- First value is the integer representing the stage.
+-- Second value is true if we need to hide this stage in the UI
+function VitalAspect:GetCurrentStage()
+end
+
+function VitalAspect:OnTimeSegmentPassETB(old_time, new_time, delta, reason)
 end
 
 function VitalAspect:HarvestStrings(t)
@@ -31,4 +41,60 @@ function VitalAspect:DeltaStat(delta)
     if self.OnDeltaStat then
         self:OnDeltaStat(old_stat, self.current_stat, delta)
     end
+end
+
+-- Define Hunger
+
+local Hunger = class( "ETBClass.Hunger", ETBClass.VitalAspect)
+Content.AddAspect( "etb_hunger", Hunger )
+
+Hunger.default_stat = 3
+Hunger.name = "Hunger"
+Hunger.desc = "Every organic being needs to eat food. Eating a healthy amount of food is key to health, strength, and not dying.",
+Hunger.loc_strings = {
+    ALT_NAME = "Hunger ({1})",
+    NAME_STAGE_1 = "Bloated",
+    NAME_STAGE_2 = "Full",
+    NAME_STAGE_3 = "Content",
+    NAME_STAGE_4 = "Peckish",
+    NAME_STAGE_5 = "Hungry",
+    NAME_STAGE_6 = "Very Hungry",
+    NAME_STAGE_7 = "Starving",
+    DESC_STAGE_1 = "<#BONUS>Bloated</>: You are quite full. Gain 1 Tiredness for every time segment passed as long as you are Bloated. Gain 3 max health when you go from Bloated to Full.",
+    DESC_STAGE_2 = "<#BONUS>Bloated</>: You have eaten enough. Gain 2 max health when you go from Full to Content.",
+    DESC_STAGE_3 = "<#HILITE>Content</>: You have eaten enough food to go for a while. No special effects.",
+    DESC_STAGE_4 = "<#HILITE>Peckish</>: You are starting to crave some food. Lose 1 resolve for each time segment passed as long as you are Peckish.",
+    DESC_STAGE_5 = "<#PENALTY>Hungry</>: You need to eat some food. Lose 1 resolve for each time segment passed as long as you are Hungry. Battle cards you own deal 1 less damage as long as you are Hungry.",
+    DESC_STAGE_6 = "<#PENALTY>Very Hungry</>: You <i>really</> need to eat some food. Lose 2 resolve and 1 max health for each time segment passed as long as you are Hungry. Battle cards you own deal 2 less damage as long as you are Hungry.",
+    DESC_STAGE_7 = "<#PENALTY>Very Hungry</>: You need to eat before you starve to death. Lose 3 resolve and 2 max health for each time segment passed as long as you are Hungry. Battle cards you own deal 3 less damage as long as you are Hungry.",
+}
+Hunger.texture = global_images.health
+
+function Hunger:GetCurrentStage()
+    if not self.current_stat then
+        return
+    end
+    if self.current_stat <= 0 then
+        return 1
+    elseif self.current_stat <= 1 then
+        return 2
+    elseif self.current_stat <= 3 then
+        return 3, true
+    elseif self.current_stat <= 5 then
+        return 4
+    elseif self.current_stat <= 7 then
+        return 5
+    elseif self.current_stat <= 9 then
+        return 6
+    elseif self.current_stat <= 11 then
+        return 7
+    end
+end
+
+function Hunger:GetName()
+    local current_stage = self:GetCurrentStage()
+    if current_stage then
+        return loc.format(self:GetLocalizedString("ALT_NAME"), self:GetLocalizedString("NAME_STAGE_" .. current_stage))
+    end
+    return Hunger._base.GetName(self)
 end
--- content/quests/main/escape_the_bog_main.lua
+++ content/quests/main/escape_the_bog_main.lua
@@ -34,7 +34,7 @@ local QDEF = QuestDef.Define
         end,
     },
 
-    AdvanceTime = function(quest, amt, source)
+    AdvanceTime = function(quest, amt, reason)
         local old_time = {datetime = TheGame:GetGameState():GetDateTime(), segment = (quest.param.time_segment or 0)}
         quest.param.time_segment = (quest.param.time_segment or 0) + amt
         while quest.param.time_segment >= 6 do
@@ -46,7 +46,7 @@ local QDEF = QuestDef.Define
             if agent.sorted_aspects then
                 for i, aspect in ipairs(agent.sorted_aspects) do
                     if aspect.OnTimeSegmentPassETB then
-                        aspect:OnTimeSegmentPassETB(old_time, new_time, amt, source)
+                        aspect:OnTimeSegmentPassETB(old_time, new_time, amt, reason)
                     end
                 end
             end
