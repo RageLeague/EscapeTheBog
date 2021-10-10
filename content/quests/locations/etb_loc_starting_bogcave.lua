2a1a950851ce8c28b16cc61faf0e34850d640f2b
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-10 17:50:48 -0400

    Update etb_loc_starting_bogcave.lua

--- content/quests/locations/etb_loc_starting_bogcave.lua
+++ content/quests/locations/etb_loc_starting_bogcave.lua
@@ -8,7 +8,7 @@ local QDEF = EscapeTheBogUtil.AddBogLocationQuest(
     },
     {
         name = "Bog Cave",
-        desc = "A cave in the middle of the bog. Not a great place to be.",
+        desc = "A cave in the middle of the bog. There does not appear to be any immediate danger, although there is not much here. If you want to survive, you should start moving.",
         plax = "INT_Bog_Cave_01",
         show_agents = true,
         tags = {"cave", "bog"},
