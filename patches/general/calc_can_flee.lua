900d543357e8a7902c6f2a64d169a746584591e0
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-18 21:32:33 -0400

    Fix bug with flee on own turn

--- patches/general/calc_can_flee.lua
+++ patches/general/calc_can_flee.lua
@@ -1,3 +1,11 @@
+local patch_id = "CALC_CAN_FLEE"
+if rawget(_G, patch_id) then
+    return
+end
+rawset(_G, patch_id, true)
+print("Loaded patch:"..patch_id)
+
+
 local battle_defs = require "battle/battle_defs"
 
 local CARD_FLAGS = battle_defs.CARD_FLAGS
@@ -6,13 +14,12 @@ local BATTLE_EVENT = ExtendEnum( battle_defs.EVENT,
     "CALC_CAN_FLEE"
 })
 
-
 local old_flee = Fighter.Flee
 function Fighter:Flee(...)
     if not self.battle.fighter_flee_accumulator then
         self.battle.fighter_flee_accumulator = CardEngine.ScalarAccumulator( self.battle, BATTLE_EVENT.CALC_CAN_FLEE )
     end
-    local can_flee, details = self.battle.fighter_kill_accumulator:CalculateValue( true, self )
+    local can_flee, details = self.battle.fighter_flee_accumulator:CalculateValue( true, self )
     if not can_flee then
         if not self:HasCondition("RUNNING") then
             self:AddCondition( "RUNNING" )
