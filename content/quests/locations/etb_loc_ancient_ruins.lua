e665301e9c6053357c6910e30a8d4f9e276926ff
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-29 18:23:25 -0400

    Update etb_loc_ancient_ruins.lua

--- content/quests/locations/etb_loc_ancient_ruins.lua
+++ content/quests/locations/etb_loc_ancient_ruins.lua
@@ -6,7 +6,7 @@ QDEF = EscapeTheBogUtil.AddBogLocationQuest(
         sleep_encounter = EscapeTheBogUtil.GenericSleepEncounterTable,
 
         on_init = function(quest)
-            -- quest.param.poi = table.arraypick{"nothing", "shrines", "ritual_platform"}
+            quest.param.poi = table.arraypick{"artifact", "shrines"}
         end,
 
         GetPathDesc = function(quest)
@@ -34,9 +34,9 @@ QDEF = EscapeTheBogUtil.AddBogLocationQuest(
 )
 
 QDEF:Loc{
-    DESC_1 = "You can see tents and campfires down this path. Someone must have been here.",
-    DESC_2 = "You can see smoke coming from this path. If you are going to find anyone, this path is your best bet.",
-    DESC_3 = "There seems to be people down this path, and they don't look very friendly. Expect confrontation down this path.",
+    DESC_1 = "There seems to be some broken buildings down this path. There might be something here.",
+    DESC_2 = "There are some man-made structures down this path, although it has seen better days.",
+    DESC_3 = "There used to be buildings down this path. But now, only a vestige of it remains.",
 }
 
 QDEF:AddConvo()
@@ -54,3 +54,42 @@ QDEF:AddConvo()
                 end )
         end
     end)
+    :State("STATE_POI")
+        :Loc{
+            DIALOG_SHRINE = [[
+                * <b>Vestige</>
+                * You find an old shrine. It doesn't look very Heshian.
+                * You have no idea what it is doing in the middle of the bog.
+                * Either way, if there is a time for some divine intervention, it should be now.
+            ]],
+            DIALOG_ARTIFACT = [[
+                * <b>Vestige</>
+                * You find a most curious looking device sitting on top of a pedestal.
+                * A shadow of a bygone era.
+                * You feel bad for it as it sits around, without a purpose, and you feel like this is a good idea to give it one.
+            ]],
+            DIALOG_AMBUSH = [[
+                * But just as you start to approach the object, you heard a mechanical whirling sound.
+                agent:
+                    !right
+                player:
+                    !left
+                    !scared
+                * Suddenly, some robots spring to life!
+                * Seems like they are guarding this location.
+                * If you want to stay here, you have to defend yourself!
+            ]],
+            DIALOG_FIGHT_WIN = [[
+                * With the last of the machine destroyed, you can now safely access this location.
+            ]],
+        }
+        :Fn(function(cxt)
+            if cxt.quest.param.poi == "shrines" then
+                cxt:Dialog("DIALOG_SHRINE")
+            else
+                cxt:Dialog("DIALOG_ARTIFACT")
+            end
+            cxt.quest.param.searched_for_poi = true
+            EscapeTheBogUtil.TryMainQuestFn("AdvanceTime", 1, "SEARCH")
+            StateGraphUtil.AddEndOption(cxt)
+        end)
