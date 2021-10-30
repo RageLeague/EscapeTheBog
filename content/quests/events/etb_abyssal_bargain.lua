0195230a1d82055e92f68deee4a135114fdb59e6
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-29 22:06:32 -0400

    Update etb_abyssal_bargain.lua

--- content/quests/events/etb_abyssal_bargain.lua
+++ content/quests/events/etb_abyssal_bargain.lua
@@ -51,12 +51,21 @@ QDEF:AddConvo(nil, nil, "SLEEP_WAKE")
                     {not player_sal?
                         * You have no idea what to expect, but depicted on this photograph is a landscape.
                         * The sun has just risen, illuminating everything.
+                        * Yet... The colors of this photograph looks dull, with very little contrast.
                     }
                 }
+                * You have no idea what to make of this.
             ]],
             OPT_TAKE_BOTTLE = "Take the bottle",
+            DIALOG_TAKE_BOTTLE = [[
+                * You take the bottle.
+                * Instinctively, you drank from it.
+                * You feel a surge of energy, a sense of grandness and power.
+                * And you crave for more.
+            ]],
         }
         :Fn(function(cxt)
+            cxt:TalkTo(cxt.player)
             cxt:Dialog("DIALOG_INTRO")
             cxt:Opt("OPT_TAKE_MEDALLION")
                 :Dialog("DIALOG_TAKE_MEDALLION")
@@ -81,8 +90,17 @@ QDEF:AddConvo(nil, nil, "SLEEP_WAKE")
                     cxt.quest.param.handler_id = TheGame:GetGameState():GetMainQuest().param.handler_id or table.arraypick(available_handlers)
                     cxt.quest.param["handler_" .. cxt.quest.param.handler_id] = true
                     TheGame:GetGameState():GetMainQuest().param.handler_id = cxt.quest.param.handler_id
-                    cxt:Dialog("DIALOG_TAKE_PHOTOGRAPH")
                 end)
+                :Dialog("DIALOG_TAKE_PHOTOGRAPH")
+                :GoTo("STATE_WAKE_UP")
+            cxt:Opt("OPT_TAKE_BOTTLE")
+                :Dialog("DIALOG_TAKE_BOTTLE")
+                :Fn(function(cxt)
+                    local character_id = cxt.player:GetContentID()
+                    TheGame:GetGameProfile():UnlockMettle( character_id )
+                    TheGame:GetGameProfile():AddMettlePoints( character_id, METTLE_POINTS, "EVENT_FIRST_METTLE" )
+                end)
+                :GoTo("STATE_WAKE_UP")
         end)
     :State("STATE_WAKE_UP")
         :Loc{
